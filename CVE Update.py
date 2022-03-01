755dc198edd4db5bf92b44eaa333fa780284ca2b
Altinser <48063738+Altinser@users.noreply.github.com>
2022-03-01 16:23:27 +0100

    Update CVE Update.py

--- CVE Update.py
+++ CVE Update.py
@@ -13,65 +13,151 @@ from selenium.webdriver.support.wait import WebDriverWait
 ser = Service(r'C:\geckodriver.exe')
 driver = webdriver.Firefox(service=ser)
 
+def create_cve_json():
+    driver.get("https://www.cvedetails.com/vulnerability-list.php?vendor_id=0&product_id=0&version_id=0&page=1&hasexp=0&opdos=0&opec=0&opov=0&opcsrf=0&opgpriv=0&opsqli=0&opxss=0&opdirt=0&opmemc=0&ophttprs=0&opbyp=0&opfileinc=0&opginf=0&cvssscoremin=9&cvssscoremax=10&year=0&month=0&cweid=0&order=1&trc=19275&sha=c560d509f935c26128bfb13d2f2dadfcea62215b")
+    number_cve = int(driver.find_element(By.XPATH, '//*[@id="pagingb"]/b').text)
+    '''
+    json_data = []
+    with open('cve.json', 'w', encoding='utf-8') as f:
+        json.dump(json_data, f, ensure_ascii=False, indent=4)
+    f.close()
+    '''
+    ok = True
+    page_num = 327
+    cve_cpt = 16350
+    while ok:
+        with open('cve.json', encoding='utf-8') as json_file:
+            json_data = json.load(json_file)
+        json_file.close()
 
-driver.get("https://www.cvedetails.com/vulnerability-list.php?vendor_id=0&product_id=0&version_id=0&page=1&hasexp=0&opdos=0&opec=0&opov=0&opcsrf=0&opgpriv=0&opsqli=0&opxss=0&opdirt=0&opmemc=0&ophttprs=0&opbyp=0&opfileinc=0&opginf=0&cvssscoremin=9&cvssscoremax=10&year=0&month=0&cweid=0&order=1&trc=19275&sha=c560d509f935c26128bfb13d2f2dadfcea62215b")
-number_cve = int(driver.find_element(By.XPATH, '//*[@id="pagingb"]/b').text)
-json_data = []
-with open('cve.json', 'w', encoding='utf-8') as f:
-    json.dump(json_data, f, ensure_ascii=False, indent=4)
+        page_num = page_num + 1
+        driver.get("https://www.cvedetails.com/vulnerability-list.php?vendor_id=0&product_id=0&version_id=0&page="+str(page_num)+"&hasexp=0&opdos=0&opec=0&opov=0&opcsrf=0&opgpriv=0&opsqli=0&opxss=0&opdirt=0&opmemc=0&ophttprs=0&opbyp=0&opfileinc=0&opginf=0&cvssscoremin=9&cvssscoremax=10&year=0&month=0&cweid=0&order=1&trc=19275&sha=c560d509f935c26128bfb13d2f2dadfcea62215b")
+        rows = driver.find_element(By.ID, "vulnslisttable").find_element(By.TAG_NAME,'tbody').find_elements(By.TAG_NAME,'tr')[1:]
+        for o in range(0,len(rows),2):
 
-ok = True
-page_num = 0
-cve_cpt = 0
-while ok:
+            cve_cpt = cve_cpt + 1
+            row = rows.pop(0)
+            link = row.find_element(By.XPATH,'td[2]/a').get_attribute('href')
+            columns = row.find_elements(By.TAG_NAME, 'td')
+            actual_cve_number = int(columns.pop(0).text)
+
+            if actual_cve_number > number_cve:
+                ok = False
+                break
+            cve_data = {'CVE_ID':'','CWE_ID':'','#':'','Vulnerability':'','Publish Date':'','Update Date':'','Score':'','Gained Access Level':'','Access':'','Complexity':'','Authentication':'','Conf':'','Integ':'','Avail':'','Description':'', 'Products Affected':[]}
+
+            cve_data['Description'] = rows.pop(0).find_element(By.TAG_NAME,'td').text
+            for i in range(0, 13):
+                cve_data[list(cve_data)[i]] = columns.pop(0).text
+
+            driver.execute_script('window.open("'+link+'","_blank");')
+            driver.switch_to.window(driver.window_handles[1])
+
+            try:
+                tab_product_affected = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "vulnprodstable"))).find_element(By.TAG_NAME,'tbody').find_elements(By.TAG_NAME,'tr')[1:]
+                for tr in tab_product_affected:
+                    data_product = {'id': '', 'Product Type': '', 'Vendor': '', 'Product': '', 'Version': '', 'Update': '', 'Edition': '','Language': ''}
+                    tds = tr.find_elements(By.TAG_NAME,'td')
+                    for j in range(0, 7):
+                        data_product[list(data_product)[j]] = tds.pop(0).text
+                    cve_data['Products Affected'].append(data_product)
+            except:
+                print("")
+            driver.close()
+            driver.switch_to.window(driver.window_handles[0])
+            print(cve_cpt,"/",number_cve)
+            json_data.append(cve_data)
+
+        with open('cve.json', 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, ensure_ascii=False, indent=4)
+        f.close()
+    driver.close()
+
+
+def cve_json_maj():
+    driver.get("https://www.cvedetails.com/vulnerability-list.php?vendor_id=0&product_id=0&version_id=0&page=1&hasexp=0&opdos=0&opec=0&opov=0&opcsrf=0&opgpriv=0&opsqli=0&opxss=0&opdirt=0&opmemc=0&ophttprs=0&opbyp=0&opfileinc=0&opginf=0&cvssscoremin=9&cvssscoremax=10&year=0&month=0&cweid=0&order=1&trc=19275&sha=c560d509f935c26128bfb13d2f2dadfcea62215b")
+    number_cve = int(driver.find_element(By.XPATH, '//*[@id="pagingb"]/b').text)
     with open('cve.json', encoding='utf-8') as json_file:
         json_data = json.load(json_file)
     json_file.close()
 
-    page_num = page_num + 1
-    driver.get("https://www.cvedetails.com/vulnerability-list.php?vendor_id=0&product_id=0&version_id=0&page="+str(page_num)+"&hasexp=0&opdos=0&opec=0&opov=0&opcsrf=0&opgpriv=0&opsqli=0&opxss=0&opdirt=0&opmemc=0&ophttprs=0&opbyp=0&opfileinc=0&opginf=0&cvssscoremin=9&cvssscoremax=10&year=0&month=0&cweid=0&order=1&trc=19275&sha=c560d509f935c26128bfb13d2f2dadfcea62215b")
-    rows = driver.find_element(By.ID, "vulnslisttable").find_element(By.TAG_NAME,'tbody').find_elements(By.TAG_NAME,'tr')[1:]
-    for o in range(0,len(rows),2):
+    ok = True
+    page_num = 0
+    cve_cpt = 0
+    while ok:
+        page_num = page_num + 1
+        driver.get("https://www.cvedetails.com/vulnerability-list.php?vendor_id=0&product_id=0&version_id=0&page=" + str(page_num) + "&hasexp=0&opdos=0&opec=0&opov=0&opcsrf=0&opgpriv=0&opsqli=0&opxss=0&opdirt=0&opmemc=0&ophttprs=0&opbyp=0&opfileinc=0&opginf=0&cvssscoremin=9&cvssscoremax=10&year=0&month=0&cweid=0&order=1&trc=19275&sha=c560d509f935c26128bfb13d2f2dadfcea62215b")
+        rows = driver.find_element(By.ID, "vulnslisttable").find_element(By.TAG_NAME, 'tbody').find_elements(By.TAG_NAME, 'tr')[1:]
+        for o in range(0, len(rows), 2):
 
-        cve_cpt = cve_cpt + 1
-        row = rows.pop(0)
-        link = row.find_element(By.XPATH,'td[2]/a').get_attribute('href')
-        columns = row.find_elements(By.TAG_NAME, 'td')
-        actual_cve_number = int(columns.pop(0).text)
+            cve_cpt = cve_cpt + 1
+            row = rows.pop(0)
+            link = row.find_element(By.XPATH, 'td[2]/a').get_attribute('href')
+            columns = row.find_elements(By.TAG_NAME, 'td')
+            actual_cve_number = int(columns.pop(0).text)
 
-        if actual_cve_number > number_cve:
-            ok = False
-            break
-        cve_data = {'CVE_ID':'','CWE_ID':'','#':'','Vulnerability':'','Publish Date':'','Update Date':'','Score':'','Gained Access Level':'','Access':'','Complexity':'','Authentication':'','Conf':'','Integ':'','Avail':'','Description':'', 'Products Affected':[]}
+            if actual_cve_number > number_cve:
+                ok = False
+                break
 
-        cve_data['Description'] = rows.pop(0).find_element(By.TAG_NAME,'td').text
-        for i in range(0, 13):
-            cve_data[list(cve_data)[i]] = columns.pop(0).text
+            cve_id = columns.pop(0).text
+            update_date = columns.pop(4).text
+            Description = rows.pop(0).find_element(By.TAG_NAME,'td').text
+            got = False
+            for cve in json_data:
+                if cve_id == cve['CVE_ID']:
+                    got = True
+                    if not update_date == cve['Update Date']:
+                        print("update",cve['CVE_ID'],cve['Update Date'])
+                        json_data.remove(cve)
+                        got = False
 
-        driver.execute_script('window.open("'+link+'","_blank");')
-        driver.switch_to.window(driver.window_handles[1])
+            if not got:
+                cve_data = {'CVE_ID': '', 'CWE_ID': '', '#': '', 'Vulnerability': '', 'Publish Date': '',
+                            'Update Date': '', 'Score': '', 'Gained Access Level': '', 'Access': '', 'Complexity': '',
+                            'Authentication': '', 'Conf': '', 'Integ': '', 'Avail': '', 'Description': '',
+                            'Products Affected': []}
 
-        try:
-            tab_product_affected = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "vulnprodstable"))).find_element(By.TAG_NAME,'tbody').find_elements(By.TAG_NAME,'tr')[1:]
-            #tab_product_affected = driver.find_element(By.ID,'vulnprodstable').find_element(By.TAG_NAME,'tbody').find_elements(By.TAG_NAME,'tr')[1:]
-            for tr in tab_product_affected:
-                data_product = {'id': '', 'Product Type': '', 'Vendor': '', 'Product': '', 'Version': '', 'Update': '', 'Edition': '','Language': ''}
-                tds = tr.find_elements(By.TAG_NAME,'td')
-                for j in range(0, 7):
-                    data_product[list(data_product)[j]] = tds.pop(0).text
-                cve_data['Products Affected'].append(data_product)
-        except:
-            print("No data")
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
-        print(cve_cpt,"/",number_cve)
-        json_data.append(cve_data)
+                cve_data['Description'] = Description
+                cve_data['CVE_ID'] = cve_id
+                cve_data['Update Date'] = update_date
+                for i in range(1, 13):
+                    if i == 5:
+                        pass
+                    else:
+                        cve_data[list(cve_data)[i]] = columns.pop(0).text
 
+                driver.execute_script('window.open("' + link + '","_blank");')
+                driver.switch_to.window(driver.window_handles[1])
+
+                try:
+                    tab_product_affected = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.ID, "vulnprodstable"))).find_element(By.TAG_NAME,
+                                                                                                'tbody').find_elements(
+                        By.TAG_NAME, 'tr')[1:]
+                    for tr in tab_product_affected:
+                        data_product = {'id': '', 'Product Type': '', 'Vendor': '', 'Product': '', 'Version': '',
+                                        'Update': '', 'Edition': '', 'Language': ''}
+                        tds = tr.find_elements(By.TAG_NAME, 'td')
+                        for j in range(0, 7):
+                            data_product[list(data_product)[j]] = tds.pop(0).text
+                        cve_data['Products Affected'].append(data_product)
+                except:
+                    print("")
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+                print(cve_cpt,cve_id,update_date, "add")
+                json_data.append(cve_data)
+
+            print(cve_cpt, "/", number_cve)
     with open('cve.json', 'w', encoding='utf-8') as f:
         json.dump(json_data, f, ensure_ascii=False, indent=4)
     f.close()
+    driver.close()
 
 
 
-driver.close()
+cve_json_maj()
+
+
 
